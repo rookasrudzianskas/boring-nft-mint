73ccd49718f09b5de1113557c7b881fad35f4697
Rokas <rookas.rudzenskas@gmail.com>
2022-09-09 16:50:19 +0300

    Rokas

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -27,7 +27,7 @@ const NFTDropPage = () => {
                 <div className="flex flex-1 flex-col p-12 lg:col-span-6">
                     <header className="flex items-center justify-between">
                         <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">The <span className="font-extrabold uppercase underline decoration-pink-600/50">Boring Apes</span> NFT Market Place</h1>
-                        <button onClick={() => connectWithMetamask()} className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">Sign In</button>
+                        <button onClick={() => address ? disconnect() : connectWithMetamask()} className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">{address ? 'Sign Out' : 'Sign In'}</button>
                     </header>
 
                     <hr className="my-2 border"/>
@@ -38,7 +38,7 @@ const NFTDropPage = () => {
                         <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
                     </div>
 
-                    <button onClick={() => connectWithMetamask()} className="h-16 w-full bg-red-600 text-white rounded-full mt-10 font-bold hover:bg-opacity-80 duration-150">
+                    <button className="h-16 w-full bg-red-600 text-white rounded-full mt-10 font-bold hover:bg-opacity-80 duration-150">
                         Mint NFT (0.1 ETH)
                     </button>
 
