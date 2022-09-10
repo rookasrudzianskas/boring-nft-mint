7e96d54c5a52dd5277896fa5a26b3ace2ed410ed
Rokas <rookas.rudzenskas@gmail.com>
2022-09-10 11:01:41 +0300

    Rokas

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -5,6 +5,7 @@ import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 import {GetServerSideProps} from "next";
 import {sanityClient, urlFor} from "../../sanity";
 import {Collection} from "../../typings";
+import Link from "next/link";
 
 interface Props {
     collections: Collection
@@ -33,7 +34,9 @@ const NFTDropPage = ({collections}: Props) => {
 
                 <div className="flex flex-1 flex-col p-12 lg:col-span-6">
                     <header className="flex items-center justify-between">
-                        <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">The <span className="font-extrabold uppercase underline decoration-pink-600/50">Boring Apes</span> NFT Market Place</h1>
+                        <Link href={'/'}>
+                            <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">The <span className="font-extrabold uppercase underline decoration-pink-600/50">Boring Apes</span> NFT Market Place</h1>
+                        </Link>
                         <button onClick={() => address ? disconnect() : connectWithMetamask()} className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">{address ? 'Sign Out' : 'Sign In'}</button>
                     </header>
 
