a6f6c27876ca7692c12c22892409d1d31d369512
Rokas <rookas.rudzenskas@gmail.com>
2022-09-10 10:46:48 +0300

    Roaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import {sanityClient, urlFor} from '../sanity';
 import {Collection} from "../typings";
 import React from "react";
+import Link from "next/link";
 
 interface Props {
     collections: Collection[]
@@ -26,13 +27,15 @@ const Home: NextPage = ({collections}: Props) => {
         <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400/20">
             <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
                 {collections.map(collection => (
-                    <div className="flex flex-col items-center hover:cursor-pointer transition-all duration-200 hover:scale-105">
-                        <img className="h-96 w-60 rounded-2xl object-cover" src={urlFor(collection.mainImage).url()} alt=""/>
-                        <div className="p-5">
-                            <h2 className="text-3xl">{collection.title}</h2>
-                            <p className="mt-2 text-sm text-gray-400">{collection.description}</p>
+                    <Link href={`/nft/${collection.slug.current}`}>
+                        <div className="flex flex-col items-center hover:cursor-pointer transition-all duration-200 hover:scale-105">
+                            <img className="h-96 w-60 rounded-2xl object-cover" src={urlFor(collection.mainImage).url()} alt=""/>
+                            <div className="p-5">
+                                <h2 className="text-3xl">{collection.title}</h2>
+                                <p className="mt-2 text-sm text-gray-400">{collection.description}</p>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </main>
