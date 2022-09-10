c54c69d4fd56207078db958094661c565a80ab2a
Rokas <rookas.rudzenskas@gmail.com>
2022-09-10 10:57:14 +0300

    Rokas

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -3,10 +3,15 @@ import Image from "next/image";
 import Head from "next/head";
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 import {GetServerSideProps} from "next";
-import {sanityClient} from "../../sanity";
+import {sanityClient, urlFor} from "../../sanity";
+import {Collection} from "../../typings";
+
+interface Props {
+    collections: Collection
+}
 
 
-const NFTDropPage = ({collections}) => {
+const NFTDropPage = ({collections}: Props) => {
     const connectWithMetamask = useMetamask();
     const address = useAddress();
     const disconnect = useDisconnect();
@@ -17,11 +22,11 @@ const NFTDropPage = ({collections}) => {
                 <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
                     <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
                         <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl">
-                            <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" src="https://links.papareact.com/8sg" alt="" />
+                            <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" src={urlFor(collections.previewImage).url()} alt="" />
                         </div>
                         <div className="text-center p-5 space-y-2">
-                            <h1 className="text-4xl font-bold text-white">Boring Apes</h1>
-                            <h2 className="text-xl text-gray-300">A collection of Boring Apes who live & breath in React</h2>
+                            <h1 className="text-4xl font-bold text-white">{collections.nftCollectionName}</h1>
+                            <h2 className="text-xl text-gray-300">{collections.description}</h2>
                         </div>
                     </div>
                 </div>
@@ -40,8 +45,8 @@ const NFTDropPage = ({collections}) => {
                     )}
 
                     <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
-                        <img src="https://links.papareact.com/bdy" className="w-80 object-cover pb-10 lg:h-40" alt=""/>
-                        <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The <span className="uppercase">Boring Apes</span> Coding Club | NFT Drop</h1>
+                        <img src={urlFor(collections.mainImage).url()} className="w-80 object-cover pb-10 lg:h-40" alt=""/>
+                        <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold"><span>{collections.title}</span></h1>
                         <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
                     </div>
 
